684cf3369cd702c5e1a67d8677b50b02b7243c70
sultondev <sultondeveloper@gmail.com>
2023-02-27 07:31:12 +0500

    fix: script setup

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script>
-import { defineComponent } from "vue";
-import TheNavbar from "@/components/TheNavbar.vue";
-
-export default defineComponent({
-  components: { TheNavbar },
-});
-</script>
-
 <template>
   <router-view />
 </template>
